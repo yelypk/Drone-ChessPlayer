56ee140af47b4221732a1c626475c7429ecc2cb1
yelyzaveta posheliuk <nemaeimya@gmail.com>
2025-07-03 14:06:41 +0300

    video reloading and image saving

---
+++ calibration.py
@@ -0,0 +1,76 @@
+import cv2
+import numpy as np 
+import glob
+import os 
+
+CHB_SIZE=(7, 7)
+CRITERIES=(cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+
+def coordination_points(board_size):
+    cop = np.zeros((board_size[0] * board_size[1], 3), np.float32)
+    cop[:, :2] = np.mgrid[0:board_size[0], 0:board_size[1]].T.reshape(-1, 2)
+    return cop
+
+def collections(image_folder, board_size):
+    imgpoints = []
+    deskpoints = []
+    image_size=[]
+
+    cop = coordination_points(board_size)
+    images = glob.glob(os.path.join(image_folder, '*.jpg'))
+    for fname in images:
+        img = cv2.imread(fname)
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        gray = cv2.equalizeHist(gray)
+
+        ret, corners = cv2.findChessboardCorners(gray, board_size, flags=cv2.CALIB_CB_ADAPTIVE_THRESH
+                                                +cv2.CALIB_CB_NORMALIZE_IMAGE)
+        image_size=gray.shape[::-1]
+
+        if ret:
+            deskpoints.append(cop)
+            corners2 = cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), CRITERIES)
+            imgpoints.append(corners2)
+            
+
+            img_drawn = cv2.drawChessboardCorners(img, board_size, corners2, ret)
+            cv2.imshow('Corners', img_drawn)
+            cv2.waitKey(100)
+
+    cv2.destroyAllWindows()
+    return deskpoints, imgpoints, image_size
+
+def calibrate_camera(deskpoints, imgpoints, image_size):
+    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(deskpoints, imgpoints, image_size, None, None)
+    return mtx, dist, rvecs, tvecs
+
+def undistort_image(image_path, mtx, dist):
+    img = cv2.imread(image_path)
+    h, w = img.shape[:2]
+    newcameramtx, roi = cv2.getOptimalNewCameraMatrix(mtx, dist, (w,h), 1, (w,h))
+
+    dst = cv2.undistort(img, mtx, dist, None, newcameramtx)
+
+    x, y, w, h = roi
+    dst = dst[y:y+h, x:x+w]
+
+    return dst
+
+def main():
+    image_folder = 'frames' 
+    example_image = 'frames/frame_2025-07-02_19-43-16-681083.jpg'
+
+    deskpoints, imgpoints, image_size = collections(image_folder, CHB_SIZE)
+    mtx, dist, rvecs, tvecs = calibrate_camera(deskpoints, imgpoints, image_size)
+
+    print("Camera matrix:\n", mtx)
+    print("Distortion coefficients:\n", dist)
+
+    np.savez('camera_calibration.npz', mtx=mtx, dist=dist)
+
+    result = undistort_image(example_image, mtx, dist)
+    cv2.imwrite('calibrated_result.jpg', result)
+    print("Saved undistorted image as calibrated_result.jpg")
+
+if __name__ == "__main__":
+    main()
---
+++ testCamera.py
@@ -0,0 +1,43 @@
+import cv2
+import os
+from datetime import datetime
+# намагаюсь не перезаписувати файл завдяки генерації імені файла за часом 
+def generate_video_filename(folder='videos', prefix='video', ext='.avi'):
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+    timestamp=datetime.now().strftime('%Y-%m-%d_%H-%M-%S-%f') # генерує унікальність часу, %f - з урахуванням мікросекунд 
+    return os.path.join(folder, f"{prefix}_{timestamp}{ext}")
+
+def capture_video():
+    cap=cv2.VideoCapture(0)
+    if not cap.isOpened():
+        print("Cant open the camera")
+        return
+
+    width=int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height=int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    fps=20.0
+
+    filename=generate_video_filename()
+    fourcc=cv2.VideoWriter_fourcc(*'XVID')
+    out=cv2.VideoWriter(filename, fourcc, fps, (width, height))
+
+    print("Press q to stop recording")
+  # цикл читання з камери 
+    while True:
+        ret, frame=cap.read()
+        if not ret:
+            break
+        out.write(frame)
+        cv2.imshow('Recording...', frame)
+
+        key=cv2.waitKey(1) & 0xFF
+        if key==ord('q'):
+            break
+        elif key==ord('s'):
+            img_filename=generate_video_filename(folder='frames', prefix='frame', ext='.jpg') #зберегання кадра як зображення 
+            cv2.imwrite(img_filename, frame)
+            print(f"Saved frame as: {img_filename}")
+capture_video()
+
+ 
